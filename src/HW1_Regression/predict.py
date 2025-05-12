3ab62ccaa44418ca8f75614efc99dd772c0c97cb
yanghy <3868850350@qq.com>
2025-05-12 11:39:31 +0800

    HW1_Regression

---
+++ src/HW1_Regression/predict.py
@@ -0,0 +1,13 @@
+from model import *
+def save_pred(preds, file):
+    ''' Save predictions to specified file '''
+    with open(file, 'w') as fp:
+        writer = csv.writer(fp)
+        writer.writerow(['id', 'tested_positive'])
+        for i, p in enumerate(preds):
+            writer.writerow([i, p])
+
+model = My_Model(input_dim=x_train.shape[1]).to(device)
+model.load_state_dict(torch.load(config['save_path']))
+preds = predict(test_loader, model, device)
+save_pred(preds, 'pred.csv')
